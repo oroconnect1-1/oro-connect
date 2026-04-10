3c4189e64899f61afb5f48fe67bdec5c65d5b470
ORO-CONNECT <oroconnect1@gmail.com>
2026-04-11 01:57:31 +0300

    Clean up sw.js by removing comments and whitespace
    Removed comments and unnecessary whitespace in service worker.

--- www/sw.js
+++ www/sw.js
@@ -6,8 +6,6 @@ const ASSETS_TO_CACHE = [
   'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Odaa_tree.png/512px-Odaa_tree.png',
   'https://cdn-icons-png.flaticon.com/512/149/149071.png'
 ];
-
-// 1. Install Event: Assets cache keessa kaa'uu
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -16,8 +14,6 @@ self.addEventListener('install', (event) => {
     })
   );
 });
-
-// 2. Activate Event: Cache moofaa qulqulleessuu
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -32,7 +28,6 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
-// 3. Fetch Event: Network yoo bade Cache irraa fiduu
 self.addEventListener('fetch', (event) => {
   if (event.request.url.includes('firestore.googleapis.com') || 
       event.request.url.includes('firebasestorage.googleapis.com')) {
@@ -41,7 +36,6 @@ self.addEventListener('fetch', (event) => {
 
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Yoo cache keessa jiraate isa deebisi, yoo hara ta'e network irraa fidi
       return response || fetch(event.request).catch(() => {
         if (event.request.mode === 'navigate') {
           return caches.match('/index.html');
@@ -50,4 +44,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
